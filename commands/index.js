6ea29481df0f829e26b96a4a3c040fb43c09f72a
Ilya Nikitsin <inikitsin@siriusiq.com>
2021-09-27 19:51:13 +0300

    add default libs

---
+++ commands/index.js
@@ -0,0 +1,11 @@
+const discord = require ("discord.js");
+
+exports.run = (client, message, args) =>{
+    const help = new discord.MessageEmbed()
+        .setColor('#da7422')
+        .setTitle('TikTok Video Downloader - Help')
+        .setURL("https://github.com/youdemilk/tiktok-discord-downloader")
+        .addFields({name: "Help", value: "Sends This Help Message" })
+        .setFooter("TikTok Video Downloader", "https://cdn.discordapp.com/app-icons/779968259671457792/184465ca744886f62b39f100b52de9a0.png?")
+    message.channel.send({embed: help })
+};
---
+++ events/ready.js
@@ -0,0 +1,5 @@
+const chalk = require("chalk")
+
+module.exports = () => {
+    console.log(chalk.green(`Welcome to Tiktok Video Downloader`));
+}
