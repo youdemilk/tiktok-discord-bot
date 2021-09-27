26d64f02c91c2f6440da3beba46899336c250262
Ilya Nikitsin <inikitsin@siriusiq.com>
2021-09-27 19:55:13 +0300

    change help message link

--- commands/index.js
+++ commands/index.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) =>{
     const help = new discord.MessageEmbed()
         .setColor('#da7422')
         .setTitle('TikTok Video Downloader - Help')
-        .setURL("https://github.com/youdemilk/tiktok-discord-downloader")
+        .setURL("https://github.com/youdemilk/tiktok-discord-bot")
         .addFields({name: "Help", value: "Sends This Help Message" })
         .setFooter("TikTok Video Downloader", "https://cdn.discordapp.com/app-icons/779968259671457792/184465ca744886f62b39f100b52de9a0.png?")
     message.channel.send({embed: help })
